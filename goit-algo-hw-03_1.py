f1550dcebaeecf6619df2db2e19ae2af02fd4e63
Oleksandr Ryzhov <154701169+OleksandrRyzhov2299@users.noreply.github.com>
2024-07-16 15:54:19 +0200

    Update goit-algo-hw-03_1.py

--- goit-algo-hw-03_1.py
+++ goit-algo-hw-03_1.py
@@ -5,28 +5,31 @@ def koch_snowflake(t, order, size):
         t.forward(size)
     else:
         for angle in [60, -120, 60, 0]:
-            size /= 3
-            koch_snowflake(t, order - 1, size)
-            t.left(60)
-            koch_snowflake(t, order - 1, size)
-            t.right(120)
-            koch_snowflake(t, order - 1, size)
-            t.left(60)
-            koch_snowflake(t, order - 1, size)
+            koch_snowflake(t, order - 1, size / 3)
             t.left(angle)
 
 def main():
     screen = turtle.Screen()
     screen.title("Сніжинка Коха")
+    screen.setup(800, 600)
+    screen.bgcolor("black")
+    
     t = turtle.Turtle()
     t.speed(0)
+    t.color("white")
+    t.penup()
+    t.goto(-300, 173)
+    t.pendown()
     
-    size = 300
+    size = 600
+    order = int(input("Введіть рівень рекурсії (рекомендовано 0-5): "))
+    
     # Створення сніжинки Коха
     for _ in range(3):
-        order = int(input("Введіть рівень рекурсії: "))
         koch_snowflake(t, order, size)
         t.right(120)
+    
+    t.hideturtle()
     screen.mainloop()
 
 if __name__ == "__main__":
